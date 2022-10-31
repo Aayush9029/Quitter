1f56cf95b821e902837fe53cb34165b08a549fc5
Aayush9029 <aayushpokharel36@gmail.com>
2022-10-31 19:21:44 -0400

    Initial Commit

---
+++ Quitter/QuitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuitterApp.swift
+//  Quitter
+//
+//  Created by Aayush Pokharel on 2022-10-31.
+//
+
+import SwiftUI
+
+@main
+struct QuitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
